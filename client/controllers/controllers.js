1c41d4073893ad5843d7cfbb77fff47c53ab4ca1
pouncealot <spamtoheck@gmail.com>
2014-11-26 10:29:30 -0800

    deleted files

--- client/controllers/controllers.js
+++
@@ -1,6 +0,0 @@
-;(function() {
-  angular.module('giraffeDraft')
-  .controller('leagueController', function() {
-
-  });
-}).call(this);
--- client/matchup/controller.js
+++
@@ -1,6 +0,0 @@
-;(function() {
-  angular.module('giraffeDraft')
-  .controller('matchupController', function() {
-
-  });
-}).call(this);
